fd8c030f4f87bc6f42f676651592e10c53daa9bc
ikswary <ikswary@naver.com>
2020-06-04 17:53:28 +0900

    update : models 함수 분리

--- API/user/models.py
+++ API/user/models.py
@@ -10,8 +10,7 @@ SELLER_ROLE_ID = 2
 
 
 def is_account_exists(db, account):
-    try:
-        cursor = db.cursor(pymysql.cursors.DictCursor)
+    with db.cursor(pymysql.cursors.DictCursor) as cursor:
         cursor.execute("""
         SELECT COUNT(*) count FROM users
         WHERE account = %s
@@ -19,61 +18,60 @@ def is_account_exists(db, account):
 
         return cursor.fetchone()['count']
 
-    finally:
-        if cursor:
-            cursor.close()
 
-
-def get_account_id(db, account):
-    try:
-        cursor = db.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("""
-        SELECT id FROM users
-        WHERE account = %s
-        """, account)
-
-        return cursor.fetchone()['id']
-
-    finally:
-        if cursor:
-            cursor.close()
-
-
-def sign_up(db, data):
-    try:
-        cursor = db.cursor()
+def insert_users(db, account):
+    with db.cursor() as cursor:
         insert_user_query = """
-            INSERT INTO users(role_id, account)
-            VALUES(%s, %s)
-            """
-        cursor.execute(insert_user_query, (SELLER_ROLE_ID, data['account']))
-        user_id = get_account_id(db, data['account'])
+        INSERT INTO users(role_id, account)
+        VALUES(%s, %s)
+        """
+        cursor.execute(insert_user_query, (SELLER_ROLE_ID, account))
 
+        return cursor.lastrowid
+
+
+def insert_user_details(db, **args):
+    with db.cursor() as cursor:
         insert_user_detail_query = """
-            INSERT INTO seller_details(
-            user_id,
-            modifier_id,
-            password,
-            seller_name,
-            seller_name_eng,
-            cs_number,
-            seller_attribute_id, site_url
-            )
-            VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
-            """
-        cursor.execute(insert_user_detail_query,
-                       (user_id,
-                        user_id,
-                        data['password'],
-                        data['seller_name'],
-                        data['seller_name_eng'],
-                        data['cs_number'],
-                        data['seller_attribute_id'],
-                        data['site_url'])
-                       )
+        INSERT INTO seller_details(
+        user_id,
+        modifier_id,
+        password,
+        seller_name,
+        seller_name_eng,
+        cs_phone,
+        seller_attribute_id,
+        site_url
+        )
+        VALUES(%(user_id)s,
+         %(user_id)s, 
+         %(password)s, 
+         %(seller_name)s, 
+         %(seller_name_eng)s, 
+         %(cs_phone)s, 
+         %(seller_attribute_id)s, 
+         %(site_url)s
+         )
+        """
+        cursor.execute(insert_user_detail_query, args)
 
-    except pymysql.err.DataError:
-        raise pymysql.err.DataError
-    finally:
-        if cursor:
-            cursor.close()
+        return cursor.lastrowid
+
+
+def insert_managers(db, phone):
+    with db.cursor() as cursor:
+        insert_manager_query = """
+        INSERT INTO managers(phone) VALUES(%s)
+        """
+        cursor.execute(insert_manager_query, phone)
+
+        return cursor.lastrowid
+
+
+def insert_user_managers(db, **args):
+    with db.cursor() as cursor:
+        insert_user_manager_query = """
+        INSERT INTO user_managers(user_detail_id, manager_id)
+        VALUES(%(user_detail_id)s, %(manager_id)s)
+        """
+        cursor.execute(insert_user_manager_query, args)
