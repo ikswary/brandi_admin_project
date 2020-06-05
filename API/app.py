e260cc0e088c1c2418e51714a175f3f4cbb96045
ikswary <ikswary@naver.com>
2020-06-05 14:18:26 +0900

    add : byte 타입을 핸들하는  커스텀 인코더 추가

--- API/app.py
+++ API/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 
+from custom_encoder import JsonCustomEncoder
 from user.controllers import user_app
 from main.controllers import main_app
 
@@ -8,6 +9,7 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config['JSON_SORT_KEYS'] = False
+    app.json_encoder = JsonCustomEncoder
     app.register_blueprint(user_app, url_prefix='/user')
     app.register_blueprint(main_app, url_prefix="/main")
     return app
