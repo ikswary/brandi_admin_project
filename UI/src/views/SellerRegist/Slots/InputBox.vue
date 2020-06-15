4116de6e5f1693c195bd4e6e42c4cf8a2c4e1a3f
ikswary <ikswary@naver.com>
2020-06-15 14:57:27 +0900

    add : 셀러 정보 수정 페이지 추가

---
+++ UI/src/views/SellerRegist/Slots/InputBox.vue
@@ -0,0 +1,36 @@
+<template>
+  <tr>
+    <th>
+      <slot name="thName"></slot>
+    </th>
+    <td>
+      <input
+        type="text"
+        :value="value"
+        @input="$emit('input',$event.target.value)"
+        :placeholder="placeholder"
+      />
+    </td>
+    <td class="infoTd">
+      <slot name="infoText01"></slot>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String
+    },
+    value: {
+      type: String
+    }
+  },
+
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+</style>
